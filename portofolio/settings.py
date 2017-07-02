747914350e225565b16d272b9507682dd72e3995
Zefarak <lirageika@hotmail.gr>
2017-07-03 00:47:28 +0300

    domain

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['illidius-corner.herokuapp.com', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['illidius-corner.herokuapp.com', '127.0.0.1:8000', 'simply-chris.com']
 
 
 # Application definition
