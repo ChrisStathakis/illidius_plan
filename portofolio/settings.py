b97bc20a3c331573d313d3c767aa17e27a6e0cb8
Zefarak <lirageika@hotmail.gr>
2017-07-09 11:05:30 +0300

    static reset

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -159,6 +159,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+
 '''
 STATIC_URL = '/static/'
 STATIC_FILES_DIRS =[os.path.join(BASE_DIR, 'static'),]
